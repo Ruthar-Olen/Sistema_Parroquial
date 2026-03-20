ee0c35fa2dbb6da5cbdce7ee53faeaf59cf5e679
Arturo Leon <fernando.arturo.leon.lopez@gmail.com>
2026-03-20 11:59:24 -0600

    catequesis v0.1

--- catequesis/admin.py
+++ catequesis/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Catequista, GrupoCatequesis
+from .models import Catequista, GrupoCatequesis, HorarioCatequesis
 
 
 @admin.register(Catequista)
@@ -13,3 +13,9 @@ class GrupoCatequesisAdmin(admin.ModelAdmin):
     list_display = ('id', 'numero_grupo', 'catequista', 'lugar', 'dia')
     search_fields = ('numero_grupo', 'catequista__nombre', 'lugar', 'dia')
     list_filter = ('dia',)
+    
+@admin.register(HorarioCatequesis)
+class HorarioCatequesisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'grupo', 'hora_inicio', 'hora_fin')
+    search_fields = ('grupo__numero_grupo',)
+    
--- catequesis/models.py
+++ catequesis/models.py
@@ -16,3 +16,11 @@ class GrupoCatequesis(models.Model):
 
     def __str__(self):
         return f"{self.numero_grupo} - {self.catequista.nombre} - {self.dia}"
+    
+class HorarioCatequesis(models.Model):
+    grupo = models.ForeignKey(GrupoCatequesis, on_delete=models.CASCADE, related_name='horarios')
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+
+    def __str__(self):
+        return f"{self.grupo.numero_grupo} - {self.hora_inicio.strftime('%H:%M')} a {self.hora_fin.strftime('%H:%M')}"
